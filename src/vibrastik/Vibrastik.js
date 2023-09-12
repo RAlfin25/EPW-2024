6828d45d318f1f4c6cead1cb6b766de0499bd556
marfanr <mohammadarfan45@gmail.com>
2023-09-12 12:51:05 +0700

    feat:update progress for  lab vibrastic (not yet)

---
+++ src/vibrastik/Vibrastik.js
@@ -0,0 +1,57 @@
+import "./vibrastik.css";
+
+function HomePage() {
+  return (
+    <>
+      <div className="header">
+        <div className="header-content">
+          <div className="header-logo">
+            <div className="logo"></div>
+            <div className="hand"></div>
+            <h3>LABORATORIUM</h3>
+            <h1>VIBRASI DAN AKUSTIK</h1>
+          </div>
+          <button>Explore More</button>
+        </div>
+        <div className="header-art"></div>
+        <div className="header-end"></div>
+      </div>
+      <div className="deskripsi-lab">
+        <div className="deskripsi-lab-content">
+          <div className="deskripsi-lab-title">
+            Apa itu Lab Vibrasi dan Akustik?
+          </div>
+          Laboratorium Vibrasi dan Akustik, Departemen Teknik Fisika, Institut
+          Teknologi Sepuluh Nopember mempelajari fenomena suara dan getaran,
+          termasuk pemrosesan pendengaran pada manusia dan penerapannya, seperti
+          speech synthesis, noise mapping, analisa getaran, dan banyak lagi
+          untuk industri dan kemanusiaan. Selain melakukan penelitian,
+          laboratorium Vibrastic juga melayani konsultasi di bidang Acoustics
+          dan getaran termasuk akustik ruangan, analisis getaran, sintesis
+          ucapan dan banyak lainnya. Sehingga Laboratorium Vibrastic teknik
+          fisika mampu bersaing dengan lembaga pendidikan dan konsultan lain
+          yang lebih besar dalam hal menggaet masyarakat untuk berkonsultasi
+          tentang masalah tersebut. Dimana menjadikan semua mahasiwa mempunyai
+          kompetensi. Laboratorium Vibrastic bersertifikat LBE (Lab Based
+          Education) oleh JICA melalui ITS – Preditc2 JICA Project.
+          <div className="baut tl" />
+          <div className="baut tr" />
+          <div className="baut bl" />
+          <div className="baut br" />
+        </div>
+      </div>
+      <div className="bidang-pengembangan">
+        <div className="content">
+          <h1>BIDANG PENGEMBANGAN</h1>
+          <h4>LABORATORIUM VIBRASI DAN AKUSTIK</h4>
+          <div className="main-content">
+            <div className="content-img"></div>
+            <div className="content-text">a</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
