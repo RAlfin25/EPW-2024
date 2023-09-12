5b7abc237965d8d430b6110933889ad76c567479
marfanr <mohammadarfan45@gmail.com>
2023-09-12 12:49:03 +0700

    feat:update progress for  lab vibrastic (not yet)

--- src/App.js
+++ src/App.js
@@ -9,16 +9,30 @@ import Merch from './merch/Merch';
 import EPC from './epc/Epc';
 import Injection from './injection/Injection';
 import Videografi from './videografi/Videografi';
+import vibras from './vibrastik/Vibrastik';
 import Footer from './component/Footer';
 import P from './component/P';
 import NotFound from './component/Notfound';
 
 
-
 function App() {
   return (
     <div className="App">
-      <NotFound />
+      <Router>
+        <Navbar/>
+        
+          <Routes>
+            <Route path='/' exact Component={HomePage}/>
+            <Route path='/aboutTF' Component={AboutTF}/>
+            <Route path='/merch' Component={Merch}/>
+            <Route path='/epc' Component={EPC}/>
+            <Route path='/injection' Component={Injection}/>
+            <Route path='/videografi' Component={Videografi}/>
+            <Route path='/vibras' Component={vibras}/>
+            
+          </Routes>
+        <Footer/>
+      </Router>
     </div>
   );
 }
